f05eea84e444c2b56f79ed0d8d7476c786753566
Nkolay Zelepukin <nzelepukin@gmail.com>
2020-03-04 09:11:08 +0300

    test heroku

---
+++ main.py
@@ -0,0 +1,23 @@
+import os, sys
+from flask import Flask, request
+from tb0t import start_bot,stop_bot
+
+if 'TELEBOT_TOKEN' not in os.environ or 'VIBER_TOKEN' not in os.environ or 'DATABASE_PG_URL' not in os.environ:
+    print('REQUIRED VARIABLES NOT SET (TELEBOT_TOKEN or VIBER_TOKEN or DATABASE_PG_URL)')
+    sys.exit()
+teletoken=os.environ['TELEBOT_TOKEN']
+server = Flask(__name__)
+
+@server.route('/tbot/' + teletoken, methods=['POST'])
+def getMessage():
+    start_bot(teletoken)
+    return "!", 200
+
+@server.route("/tbot/")
+def webhook():
+    stop_bot(teletoken)
+    return "!", 200
+
+if __name__ == '__main__':
+    server.debug = True
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
--- tb0t.py
+++ tb0t.py
@@ -1,12 +1,9 @@
-import os,telebot,sys,json
-from sqlalchemy import Table, Column, Integer, String, Float,MetaData, ForeignKey, engine, create_engine
+import os,telebot,sys,json,time
+from sqlalchemy import Table, Column,DateTime, Integer, String, Float,LargeBinary, MetaData, ForeignKey, engine, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker,relationship
-from flask import Flask, request
 
-server = Flask(__name__)
 
-token=os.environ['TELEBOT_TOKEN']
 keyboard1=telebot.types.ReplyKeyboardMarkup(True,True)
 keyboard1.row('/add','/list','/reset')
 bot = telebot.TeleBot(token)
@@ -17,35 +14,61 @@ db_url=os.environ['DATABASE_URL']
 engine=create_engine(db_url)
 Base = declarative_base()
 metadata = MetaData()
-class Teleuser(Base):
-    __tablename__='teleuser'
+
+class Userinfo(Base):
+    __tablename__='userinfo'
     id = Column(Integer, primary_key=True)
-    username = Column( String(100))
-    last_request = Column(Integer)
-    locations = relationship("Location", back_populates="teleuser",cascade="all, delete, delete-orphan")
-class Location(Base):
-    __tablename__='location'
+    username = Column( String(20))
+    user_fio = Column( String(100))
+    messanger = Column( String(20))
+    logs = relationship("UserLog", back_populates="user",cascade="all, delete, delete-orphan")
+
+class UserLog(Base):
+    __tablename__='userlog'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey("teleuser.id"))
+    user_id = Column(Integer, ForeignKey("userinfo.id"))
+    date=Column(DateTime)
+    #place_id = Column(Integer, ForeignKey("place.id"))
+    user = relationship("Userinfo", back_populates="logs")
+'''
+class Place(Base):
+    __tablename__='place'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("userinfo.id"))
+    util_type=Column( String(20))
     loc_lat = Column(Float)
     loc_lon = Column(Float)
-    teleuser = relationship("Teleuser", back_populates="locations")
-
+    text = Column( String(100))
+    photo = Column(LargeBinary)
+'''
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
-def insert_user(user:str)->None:
-    ''' Save USER in Postgres DB '''
+
+def insert_log(user:str, place:str):
+    ''' Save log'''
     session=Session()
-    user_list=[i[0] for i in session.query(Teleuser.username)]
-    if not user in user_list:
-        db_user= Teleuser(username=user)
-        session.add(db_user)
-        session.commit()
-    else: print ('User {} already in base'.format(user))
+    db_user = session.query(Userinfo).filter(Userinfo.username==user).one()
+    db_log =UserLog ( user_id=db_user.id, date= time.ctime())
+    session.add(db_log)
+    session.commit()   
     session.close()
 
+def insert_user(user:str,message)->None:
+    ''' Save USER in Postgres DB '''
+    session=Session()
+    user_list=[i[0] for i in session.query(Userinfo.username)]
+    if not user in user_list:
+        db_user= Userinfo(  username=user, 
+                            user_fio = '{} {}'.format(message.from_user.first_name, message.from_user.last_name),
+                            messanger = 'telegram')
+        session.add(db_user)
+    else: 
+        print ('User already in base')
+    session.commit()   
+    session.close()
+'''
 def insert_location(lat:float,lon:float,user:str)->None:
-    ''' Save LOCATION (latitude, longitude) in Postgres DB '''
+    # Save LOCATION (latitude, longitude) in Postgres DB 
     session=Session()
     user_list=[i[0] for i in session.query(Teleuser.username)]
     if user in user_list:
@@ -55,13 +78,13 @@ def insert_location(lat:float,lon:float,user:str)->None:
         session.commit()
     else: print ('Cant find {} in base'.format(user))
     session.close()
-
+'''
 def delete_user(user):
     ''' Delete USER from Postgres and all his locations '''
     session=Session()
-    user_list=[i[0] for i in session.query(Teleuser.username)]
+    user_list=[i[0] for i in session.query(Userinfo.username)]
     if user in user_list:
-        myuser = session.query(Teleuser).filter(Teleuser.username==user).one()
+        myuser = session.query(Userinfo).filter(Userinfo.username==user).one()
         session.delete(myuser)
         session.commit()
     else: print ('Cant find {} in base'.format(user))
@@ -84,20 +107,20 @@ def start_message(message):
 @bot.message_handler(commands=['reset'])
 def reset_message(message):
     session=Session()
-    user_list=[i[0] for i in session.query(Teleuser.username)]
+    user_list=[i[0] for i in session.query(Userinfo.user_id)]
     session.close()
     if not str(message.chat.id) in user_list: 
         bot.send_message(message.chat.id,'Пока данных по вам нет',reply_markup=keyboard1)
     else: 
         delete_user(str(message.chat.id))
         bot.send_message(message.chat.id,'Ваши данные успешно удалены',reply_markup=keyboard1)
-
-@bot.message_handler(commands=['list'])
+'''
+@bot.message_handler(commands=['users_list'])
 def list_message(message):
     session=Session()
     user_list=[i[0] for i in session.query(Teleuser.username)]
     if not str(message.chat.id) in user_list:
-        bot.send_message(message.chat.id,'Пока данных по вам нет',reply_markup=keyboard1)
+        bot.send_message(message.chat.id,'Пока данных нет',reply_markup=keyboard1)
     else:
         myuser = session.query(Teleuser).filter(Teleuser.username==str(message.chat.id)).one()
         loc_list=[{'lat':i,'lon':j} for i,j in session.query(Location.loc_lat,Location.loc_lon).filter(Location.user_id==myuser.id)]
@@ -110,13 +133,13 @@ def list_message(message):
     session.close()
     bot.send_message(message.chat.id,'-= END =-',reply_markup=keyboard1) 
 
-
+'''
 @bot.message_handler(commands=['add'])
 def add_message(message):
     bot.send_message(message.chat.id,'Привет, введи место')
-    insert_user(str(message.chat.id))
+    insert_user(str(message.from_user.id),message)
 
-
+'''
 @bot.message_handler(content_types=['location'])
 def get_location(message):
     session=Session()
@@ -129,19 +152,12 @@ def get_location(message):
         insert_location(message.location.latitude,message.location.longitude,str(message.chat.id))
         bot.send_message(message.chat.id,'Мастонахождение добавлено',reply_markup=keyboard1)
 '''
-@server.route('/' + token, methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://ecoukhta.herokuapp.com/' + token)
-    return "!", 200
+    def start_bot(token):
+        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    
+    def stop_bot(token):
+        bot.remove_webhook()
+        bot.set_webhook(url='https://ecoukhta.herokuapp.com/' + token)
 
 
-if __name__ == '__main__':
-    server.debug = True
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
-'''
+bot.polling()
