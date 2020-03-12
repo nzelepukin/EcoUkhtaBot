4db86b60e50ef6575698e47c35e7893cd8e87b68
Nkolay Zelepukin <nzelepukin@gmail.com>
2020-03-12 22:18:15 +0300

    add photos in vk

--- vkb0t.py
+++ vkb0t.py
@@ -19,7 +19,7 @@ def start_vk():
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             user_info={'username':event.user_id, 'messanger':'vk'}
-            user_res=vk_session.method("users.getById",{'user_ids':[user_info['username']],'fields':['id','first_name','last_name']})
+            user_res=vk_session.method("users.get",{'user_ids':[user_info['username']],'fields':['id','first_name','last_name']})
             print(user_res['items'])
             result = vk_session.method("messages.getById", {"message_ids": [event.message_id],"group_id": 192738048})
             if result['items'][0]['geo']:
